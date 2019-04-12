426a8b25f9d254807ddbdc0a2677d7567a6c68e3
jack <jackkaback@gmail.com>
2019-04-12 18:23:12 -0400

    Added stuff like input selection

--- src/main.java
+++ src/main.java
@@ -1,14 +1,40 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
+import javax.swing.*;
+import java.util.*;
 
 public class main {
 	public static void main(String[] args){
+		
+		// For input selection
+		String[] values = {"Load from file", "Enter manually", "Quit"};
+		Object selected;
 
 		Scanner input = new Scanner(System.in);
 
-		Map<String, Integer> words = new HashMap<String, Integer>();
+		HashMap<String, Integer> words = new HashMap<String, Integer>();
+
+		while(true) {
+			selected = JOptionPane.showInputDialog(null, "What is the target Nicotine level?", "Selection", JOptionPane.DEFAULT_OPTION, null, values, "0");
+
+			// adds words found from a txt file
+			if(selected == "Load from file"){
+			}
+			// Adds manually entered ords to the list
+			else if(selected == "Enter manually") {
+				words.putAll(getManualEntry(input));
+			}
+
+			// exits
+			else if(selected == "Quit"){
+				break;
+			}
+		}
+		words = sortByValue((HashMap<String, Integer>) words);
+		printMap(words);
+	}
+
+	public static HashMap<String, Integer> getManualEntry(Scanner input) {
+
+		HashMap<String, Integer> startMap = new HashMap<String, Integer>();
 
 		//adds to the hashmap
 		while(true) {
@@ -22,15 +48,18 @@ public class main {
 			String[] array = temp.split(" ");
 
 			for (int i = 0; i < array.length; i++) {
-				if (words.containsKey(array[i])) {
-					words.put(array[i], words.get(array[i]) + 1);
+				if (startMap.containsKey(array[i])) {
+					startMap.put(array[i], startMap.get(array[i]) + 1);
 				} else {
-					words.put(array[i], 1);
+					startMap.put(array[i], 1);
 				}
 			}
 		}
-		printMap(words);
+
+		return startMap;
+
 	}
+
 
 	// Prints all the hashmap
 	public static void printMap(Map mp) {
@@ -40,6 +69,20 @@ public class main {
 			System.out.println(pair.getKey() + " = " + pair.getValue());
 			it.remove();
 		}
+	}
+
+	// returns the new hashmap
+	public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> unsortedArray) {
+		//LinkedHashMap preserve the ordering of elements in which they are inserted
+		LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
+
+		//Use Comparator.reverseOrder() for reverse ordering
+		unsortedArray.entrySet()
+				.stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+				.forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
+
+		return reverseSortedMap;
 	}
 
 }
